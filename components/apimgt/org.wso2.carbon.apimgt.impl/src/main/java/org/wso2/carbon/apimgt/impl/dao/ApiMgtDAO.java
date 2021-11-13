d4b21f01cf4946ef71388491b5cb5cc1b60e2661
msm1992 <madusha.sachini@yahoo.com>
2021-11-13 07:41:15 +0530

    Remove CHANGE_ENDPOINT policy

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -5759,10 +5759,8 @@ public class ApiMgtDAO {
                     if (uriTemplate.getOperationPolicies() != null) {
                         for (OperationPolicy policy : uriTemplate.getOperationPolicies()) {
                             String policyType = policy.getPolicyType().toString();
-                            if (!StringUtils.isEmpty(policyType) && (
-                                    policyType.equalsIgnoreCase(OperationPolicy.PolicyType.CHANGE_ENDPOINT.toString())
-                                            || policyType.equalsIgnoreCase(
-                                            OperationPolicy.PolicyType.CALL_VALIDATION_SERVICE.toString()))) {
+                            if (!StringUtils.isEmpty(policyType) && (policyType.equalsIgnoreCase(
+                                    OperationPolicy.PolicyType.CALL_INTERCEPTOR_SERVICE.toString()))) {
                                 endpointPolicyExists = true;
                             }
                             Gson gson = new Gson();
@@ -14533,9 +14531,7 @@ public class ApiMgtDAO {
                                 insertOperationPolicyMappingStatement.executeUpdate();
 
                                 String policyType = policy.getPolicyType().toString();
-                                String tenantDomain = APIUtil.getTenantDomainFromTenantId(tenantId);
-                                if (OperationPolicy.PolicyType.CHANGE_ENDPOINT.toString().equals(policyType)
-                                        || OperationPolicy.PolicyType.CALL_VALIDATION_SERVICE.toString()
+                                if (OperationPolicy.PolicyType.CALL_INTERCEPTOR_SERVICE.toString()
                                         .equals(policyType)) {
                                     //copy duplicate resource endpoint record for API Product
                                     String endpointId = (String) policy.getParameters()
@@ -16374,10 +16370,8 @@ public class ApiMgtDAO {
                             if (urlMapping.getOperationPolicies().size() > 0) {
                                 for (OperationPolicy policy : urlMapping.getOperationPolicies()) {
                                     String policyType = policy.getPolicyType().toString();
-                                    if (!StringUtils.isEmpty(policyType) && (policyType
-                                            .equalsIgnoreCase(OperationPolicy.PolicyType.CHANGE_ENDPOINT.toString())
-                                            || policyType.equalsIgnoreCase(
-                                            OperationPolicy.PolicyType.CALL_VALIDATION_SERVICE.toString()))) {
+                                    if (StringUtils.isEmpty(policyType) && policyType.equalsIgnoreCase(
+                                            OperationPolicy.PolicyType.CALL_INTERCEPTOR_SERVICE.toString())) {
                                         endpointPolicyExists = true;
                                     }
 
@@ -17188,10 +17182,8 @@ public class ApiMgtDAO {
                             while (rs.next()) {
                                 for (OperationPolicy policy : urlMapping.getOperationPolicies()) {
                                     String policyType = policy.getPolicyType().toString();
-                                    if (!StringUtils.isEmpty(policyType) && (policyType
-                                            .equalsIgnoreCase(OperationPolicy.PolicyType.CHANGE_ENDPOINT.toString())
-                                            || policyType.equalsIgnoreCase(
-                                            OperationPolicy.PolicyType.CALL_VALIDATION_SERVICE.toString()))) {
+                                    if (!StringUtils.isEmpty(policyType) && policyType.equalsIgnoreCase(
+                                            OperationPolicy.PolicyType.CALL_INTERCEPTOR_SERVICE.toString())) {
                                         endpointPolicyExists = true;
                                     }
 
@@ -17509,9 +17501,7 @@ public class ApiMgtDAO {
                                 insertOperationPolicyMappingStatement.executeUpdate();
 
                                 String policyType = policy.getPolicyType().toString();
-                                String tenantDomain = APIUtil.getTenantDomainFromTenantId(tenantId);
-                                if (OperationPolicy.PolicyType.CHANGE_ENDPOINT.toString().equals(policyType)
-                                        || OperationPolicy.PolicyType.CALL_VALIDATION_SERVICE.toString()
+                                if (OperationPolicy.PolicyType.CALL_INTERCEPTOR_SERVICE.toString()
                                         .equals(policyType)) {
                                     String endpointId = (String) policy.getParameters()
                                             .get(APIConstants.ENDPOINT_ID_PARAM);
@@ -17775,9 +17765,7 @@ public class ApiMgtDAO {
                                     addOperationPolicyStatement.executeUpdate();
 
                                     String policyType = policy.getPolicyType().toString();
-                                    String tenantDomain = APIUtil.getTenantDomainFromTenantId(tenantId);
-                                    if (OperationPolicy.PolicyType.CHANGE_ENDPOINT.toString().equals(policyType)
-                                            || OperationPolicy.PolicyType.CALL_VALIDATION_SERVICE.toString()
+                                    if (OperationPolicy.PolicyType.CALL_INTERCEPTOR_SERVICE.toString()
                                             .equals(policyType)) {
                                         String endpointId = (String) policy.getParameters()
                                                 .get(APIConstants.ENDPOINT_ID_PARAM);
